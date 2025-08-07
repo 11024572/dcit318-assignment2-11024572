0dc2bc761801dec0c7e68f0d7947bfc4ba78ea23
Caleb <ckdanquah002@st.ug.edu.gh>
2025-08-07 10:04:42 +0000

    Your commit message

--- InheritanceDemo/Program.cs
+++ InheritanceDemo/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace InheritanceDemo
+{
+    // Base class
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    // Derived class 1
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    // Derived class 2
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal genericAnimal = new Animal();
+            Animal dog = new Dog();
+            Animal cat = new Cat();
+
+            genericAnimal.MakeSound(); // Output: Some generic sound
+            dog.MakeSound();           // Output: Bark
+            cat.MakeSound();           // Output: Meow
+        }
+    }
+}
