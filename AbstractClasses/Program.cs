398c6a4e802a33076be36d2a81711ed622108d5b
Caleb <ckdanquah002@st.ug.edu.gh>
2025-08-09 12:31:49 +0000

    AbstractClasses

--- AbstractClasses/Program.cs
+++ AbstractClasses/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ShapesExample
+{
+    // Abstract class Shape
+    public abstract class Shape
+    {
+        // Abstract method to get area
+        public abstract double GetArea();
+    }
+
+    // Circle class inheriting from Shape
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    // Rectangle class inheriting from Shape
+    public class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    // Main program
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a Circle with radius 5
+            Shape circle = new Circle(5);
+            Console.WriteLine($"Circle Area: {circle.GetArea()}");
+
+            // Create a Rectangle with width 4 and height 6
+            Shape rectangle = new Rectangle(4, 6);
+            Console.WriteLine($"Rectangle Area: {rectangle.GetArea()}");
+
+            Console.ReadLine();
+        }
+    }
+}
