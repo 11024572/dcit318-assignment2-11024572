a6b654c6cbe3c25284fee5a6b46cda8e7169ae77
Caleb <ckdanquah002@st.ug.edu.gh>
2025-08-09 12:38:11 +0000

    Interfaces

--- AbstractClasses/Interfaces/Program.cs
+++ AbstractClasses/Interfaces/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MovableApp
+{
+    // Interface definition
+    public interface IMovable
+    {
+        void Move();
+    }
+
+    // Car class implementing IMovable
+    public class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    // Bicycle class implementing IMovable
+    public class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    // Main program
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMovable car = new Car();
+            car.Move();
+
+            IMovable bicycle = new Bicycle();
+            bicycle.Move();
+
+            Console.ReadLine();
+        }
+    }
+}
